e28dd9a9b6a533fd03fb81eb02ed8c3d71568792
MIKA BE <mikushbe@gmail.com>
2023-12-07 00:06:30 -0500

    test2

--- routes/login2.js
+++ routes/login2.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'The Langenheim', 
+  res.render('login2', { title: 'The Langenheim', 
                         layout: 'layout'});
 });
 
