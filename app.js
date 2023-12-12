9625281a2855fe12b835c5aa0ea13af610eac308
mikabe1805 <mikushbe@gmail.com>
2023-12-12 09:45:58 -0500

    experiment2

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/artwork', artworkRouter);
 app.use('/about', aboutRouter);
 app.use('/credits', creditsRouter);
