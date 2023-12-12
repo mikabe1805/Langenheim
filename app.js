747c94221b2f99756b31f694a3a16e7dedb43dae
MIKA BE <mikushbe@gmail.com>
2023-12-11 22:13:55 -0500

    test3

--- app.js
+++ app.js
@@ -148,13 +148,13 @@ const config = {
   issuerBaseURL: 'https://dev-lkgc5j11.us.auth0.com'
 };
 
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-// // req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 
 
