1dff1162b1d159477e329e0af885defb2728857c
mikabe1805 <mikushbe@gmail.com>
2023-12-12 09:37:32 -0500

    try again!!

--- app.js
+++ app.js
@@ -152,9 +152,9 @@ const config = {
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
 // const { requiresAuth } = require('express-openid-connect');
 
