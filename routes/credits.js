b2ba6bd771989656abbd7bb2da1d287efab8d08e
mikabe1805 <mikushbe@gmail.com>
2023-12-13 11:49:50 -0500

    the real test

--- routes/credits.js
+++ routes/credits.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// var data = require('../data/data');
+var data = require('../data/data');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
