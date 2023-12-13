1d0e2f7073b686346562c381b3788bf75a769c6d
mikabe1805 <mikushbe@gmail.com>
2023-12-13 11:38:27 -0500

    checking something

--- routes/credits.js
+++ routes/credits.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 var data = require('../data/data');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  let artworkData = await data.getAllArtwork();
-  let artwork = artworkData.artwork;
-  //Sort by last name
-  this.artwork = artwork.sort(function (a, b) {
-    let name_a = a.art_creator.split(' ')[1];
-    let name_b = b.art_creator.split(' ')[1];
-    return name_a.localeCompare(name_b);
-  });
+// /* GET home page. */
+// router.get('/', async function(req, res, next) {
+//   let artworkData = await data.getAllArtwork();
+//   let artwork = artworkData.artwork;
+//   //Sort by last name
+//   this.artwork = artwork.sort(function (a, b) {
+//     let name_a = a.art_creator.split(' ')[1];
+//     let name_b = b.art_creator.split(' ')[1];
+//     return name_a.localeCompare(name_b);
+//   });
 
-  let names = [];
+//   let names = [];
 
-  for (i = 0; i < artwork.length; i++){
-    if(!(names.indexOf(artwork[i].art_creator) + 1)){
-      names.push(artwork[i].art_creator);
-    }
-  }
+//   for (i = 0; i < artwork.length; i++){
+//     if(!(names.indexOf(artwork[i].art_creator) + 1)){
+//       names.push(artwork[i].art_creator);
+//     }
+//   }
 
-  res.render('credits', { title: 'The Langenheim', 
-                          layout: 'layout',
-                          names: names});
-});
+//   res.render('credits', { title: 'The Langenheim', 
+//                           layout: 'layout',
+//                           names: names});
+// });
 
-module.exports = router;
+// module.exports = router;
