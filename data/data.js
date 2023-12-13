3318ccf34d89897f9ed94ee080ba26af5b763501
mikabe1805 <mikushbe@gmail.com>
2023-12-13 12:31:11 -0500

    undoing what i did for a moment

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ var {google} = require('googleapis');
 // File handling package
 const fs = require('fs');
 const { INSPECT_MAX_BYTES } = require('buffer');
-const RESPONSES_SHEET_ID = '11dCOw0eFFKVXmPo1alL-G006VF7_xHL0VLrYNVsnkGM';
+const RESPONSES_SHEET_ID = '1VEIONwFJ0TQzdLZX41bddhHmM1eNxbRyCiBP2KaYNZA';
 
 
 const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
@@ -11,7 +11,7 @@ const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
 const CREDENTIALS = JSON.parse(fs.readFileSync('./credentials.json'));
 
 const getServerSide = async() => {
-  const auth = await google.auth.getClient({scopes: ['https://www.googleapis.com/auth/spreadsheets']});
+  const auth = await google.auth.getClient({scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']});
   const sheets = google.sheets({ version: 'v4', auth });
 }
 
@@ -70,40 +70,40 @@ module.exports.getAllArtwork = async (data) => {
     // load the documents info
     await doc.loadInfo();
 
-    // let sheet = doc.sheetsByIndex[0];
-    // let tags = [];
-    // let SKIP_TAGS = ["Freshman", "Sophomore", "Junior", "Senior"]
+    let sheet = doc.sheetsByIndex[0];
+    let tags = [];
+    let SKIP_TAGS = ["Freshman", "Sophomore", "Junior", "Senior"]
 
-    // let rows = await sheet.getRows();
-    // let jsonObj = [];
-    // for (let index = 0; index < rows.length; index++) {
-    //     var row = rows[index];
-    //     if(row.Valid === "TRUE"){
-    //         item = {};
-    //         item ["art_title"] = row.Artwork_Name;
-    //         item ["art_creator"] = row.Artist_Name;
-    //         item ["art_description"] = row.Description;;
-    //         artsourcelink = row.Upload_Artwork;
-    //         baseUrl = "https://drive.google.com/uc?id"; // back to normal for now
-    //         imageId = artsourcelink.substr(32, 34); //this will extract the image ID from the shared image link
-    //         url = baseUrl.concat(imageId);
-    //         item ["art_source"] = url;
-    //         item ["art_id"] = row.ID;
-    //         item ["art_type"] = row.Media_Format;
-    //         item ["art_tags"] = formatTags(row.Tags);
-    //         item["art_tags"].forEach(function (tag){
-    //         if(!containsTag(tag, tags)){
-    //             tags.push(tag);
-    //         }
-    //         });
-    //         item ["row_number"] = row._rowNumber;
+    let rows = await sheet.getRows();
+    let jsonObj = [];
+    for (let index = 0; index < rows.length; index++) {
+        var row = rows[index];
+        if(row.Valid === "TRUE"){
+            item = {};
+            item ["art_title"] = row.Artwork_Name;
+            item ["art_creator"] = row.Artist_Name;
+            item ["art_description"] = row.Description;;
+            artsourcelink = row.Upload_Artwork;
+            baseUrl = "https://drive.google.com/uc?id"; // back to normal for now
+            imageId = artsourcelink.substr(32, 34); //this will extract the image ID from the shared image link
+            url = baseUrl.concat(imageId);
+            item ["art_source"] = url;
+            item ["art_id"] = row.ID;
+            item ["art_type"] = row.Media_Format;
+            item ["art_tags"] = formatTags(row.Tags);
+            item["art_tags"].forEach(function (tag){
+            if(!containsTag(tag, tags)){
+                tags.push(tag);
+            }
+            });
+            item ["row_number"] = row._rowNumber;
 
-    //         if(!(containsID(jsonObj, row.ID))){
-    //           jsonObj.push(item);
-    //         }
-    //     }
+            if(!(containsID(jsonObj, row.ID))){
+              jsonObj.push(item);
+            }
+        }
 
-    // };
+    };
     return {artwork: jsonObj, tags: tags};
 };
 
