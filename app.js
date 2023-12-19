1dea2b0e5d63ac9abe1020d197c46eece2eef304
mikabe1805 <mikushbe@gmail.com>
2023-12-19 09:13:19 -0500

    get rid of data for now

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ const { requiresAuth } = require('express-openid-connect');
 app.get('/profile', requiresAuth(), async function(req, res) {
   //res.send(JSON.stringify(req.oidc.user));
   let user = req.oidc.user;
-  let art = await data.getArtwork("Lauren Kim");
+  //let art = await data.getArtwork("Lauren Kim");
   res.render('profile', { title: 'The Langenheim', 
                         layout: 'layout',
                         // art: art,
