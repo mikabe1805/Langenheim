84246fc9488fd3591df9d7781700a1f3495145d0
mikabe1805 <mikushbe@gmail.com>
2024-03-06 15:33:39 -0500

    ill have to get back to it later

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var data = require('../data/data')
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   // let artID = Math.random() * data.maxID;
-  let artID = 1;
-  let art = await data.getArtwork(artID);
+  // let artID = 1;
+  let artt = await data.getArtwork(1);
   res.render('index', { title: 'The Langenheim', 
-                        art: art,
+                        art: artt,
                         layout: 'layout'});
 });
 
