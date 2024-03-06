c8abb5f8ad8504b273e1f9321097e02e7b02ea92
mikabe1805 <mikushbe@gmail.com>
2024-03-06 13:06:32 -0500

    checking that everything works so far

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/data')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  let artID = Math.random() * data.maxID;
+  let art = await data.getArtwork(artID);
   res.render('index', { title: 'The Langenheim', 
+                        art: art,
                         layout: 'layout'});
 });
 
