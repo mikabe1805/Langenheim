c34251e530ea3f927dd5414d9035e31a320d8a50
MIKA BE <mikushbe@gmail.com>
2024-03-08 01:40:27 -0500

    idk anymore

--- routes/loadUnity.js
+++ routes/loadUnity.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-// const cors=require("cors");
-// const corsOptions ={
-//    origin:'*', 
-//    credentials:true,            //access-control-allow-credentials:true
-//    optionSuccessStatus:200,
-// }
+// // const cors=require("cors");
+// // const corsOptions ={
+// //    origin:'*', 
+// //    credentials:true,            //access-control-allow-credentials:true
+// //    optionSuccessStatus:200,
+// // }
 
-// app.use(cors(corsOptions)) // Use this after the variable declaration
+// // app.use(cors(corsOptions)) // Use this after the variable declaration
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,3 +16,12 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+var x = new XMLHttpRequest();
+x.open('GET', 'https://mysterious-citadel-11464-8a0e45efb7c9.herokuapp.com/https://langenheim-a07134ab155c.herokuapp.com/loadUnity');
+// I put "XMLHttpRequest" here, but you can use anything you want.
+x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+x.onload = function() {
+    alert(x.responseText);
+};
+x.send();
