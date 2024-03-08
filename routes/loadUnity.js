c1001e5309a2a30dfbeb02ec3fdf1143638461b5
MIKA BE <mikushbe@gmail.com>
2024-03-07 23:59:20 -0500

    idk if this will work

--- routes/loadUnity.js
+++ routes/loadUnity.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
