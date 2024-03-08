63eb591e1eb6c5b4924fdcf3bb9f5ad8268031a8
MIKA BE <mikushbe@gmail.com>
2024-03-08 00:03:07 -0500

    urgh

--- routes/loadUnity.js
+++ routes/loadUnity.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const cors=require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+// app.use(cors(corsOptions)) // Use this after the variable declaration
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
